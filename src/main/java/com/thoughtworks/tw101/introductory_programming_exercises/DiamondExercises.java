8c648cd444e8232eff5bab4b2b5e545cbc75291d
angelicaperez37 <angelica.perez37@gmail.com>
2017-07-17 23:36:48 -0700

    Completed DiamondExercises.

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -13,7 +13,23 @@ public class DiamondExercises {
 //             ***
 //            *****
     private static void drawAnIsoscelesTriangle(int n) {
+        for (int i = n-1; i >= 0; i--) {
+            drawShapeRow(i, n);
+        }
+    }
 
+    // prints n spaces
+    private static void printSpaces(int n) {
+        for (int j = 0; j < n; j++) {
+            System.out.print(" ");
+        }
+    }
+
+    // prints n asterisks
+    private static void printAsterisks(int n) {
+        for (int j = 0; j < n; j++) {
+            System.out.print("*");
+        }
     }
 
 //    Diamond
@@ -24,9 +40,19 @@ public class DiamondExercises {
 //             ***
 //              *
     private static void drawADiamond(int n) {
-
+        drawAnIsoscelesTriangle(n);
+        for (int i = 1; i < n; i++) {
+            drawShapeRow(i, n);
+        }
     }
 
+    // draws row of a shape with i spaces on each end
+    // for a shape(triangle) of height n
+    private static void drawShapeRow(int i, int n) {
+        printSpaces(i);
+        printAsterisks((n-i-1)*2+1);
+        System.out.println();
+    }
 //    Diamond with Name
 //    Given a number n, print a centered diamond with your name in place of the middle line. Example for n=3:
 //
@@ -36,6 +62,22 @@ public class DiamondExercises {
 //            ***
 //             *
     private static void drawADiamondWithYourName(int n) {
-
+        int rowOffset = 0;
+        int nameOffset = 0;
+        if (n*2 < 7) {
+            rowOffset = (8-n*2)/2;
+        } else {
+            nameOffset = (n*2-8)/2;
+        }
+        for (int i = n-1; i > 0; i--) {
+            printSpaces(rowOffset);
+            drawShapeRow(i, n);
+        }
+        printSpaces(nameOffset);
+        System.out.println("Angelica");
+        for (int i = 1; i < n; i++) {
+            printSpaces(rowOffset);
+            drawShapeRow(i, n);
+        }
     }
 }
